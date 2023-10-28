d511a72336d3fc7d360e8c389dd005079e6621ff
0613jyh <0613jyh@naver.com>
2023-10-28 20:58:48 +0900

    [baekjoon] {prob팰린드롬수리: {memory} KB, 시간: {ti1116

---
+++ baekjoon/1259 팰린드롬수.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <string.h>
+
+
+int main() {
+
+	char str[5] = { 0, }; // 123 
+
+	while (1) {
+		scanf("%s", str);
+		
+		if (str[0] == '0') {
+			break;
+		}
+		int len = strlen(str);
+		int arr = len - 1;
+		int count = len / 2;
+		int flag = 0;
+		for (int i = 0; i < count; i++) {
+
+			if (str[i] == str[arr]) {
+				flag++;
+				arr--;
+			}
+			else {
+				continue;
+			}
+		}
+
+		if (flag >= count) {
+			printf("yes\n");
+		}
+		else {
+			printf("no\n");
+		}
+	}
+}
