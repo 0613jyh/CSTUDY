490de0cb5e8d8e83d4f9e182ebe1cc2953028619
0613jyh <0613jyh@naver.com>
2023-10-28 21:00:22 +0900

    [baekjoon] {prob단어 공부리: {memory} KB, 시간: {ti1972

---
+++ baekjoon/1157 단어 공부.c
@@ -0,0 +1,51 @@
+
+	#include <stdio.h>
+	#include <string.h>
+	#define max 1000000
+
+	int main() {
+	
+		char ch[max] = { 0, };
+		int score[26] = { 0, };
+		int index = 0;
+		int flag = 0;
+
+
+
+		scanf("%s", ch);
+
+		int len = strlen(ch);
+
+		for (int i = 0; i < len; i++) {
+			if (ch[i] >= 'a') {
+				score[ch[i] - 97]++;
+			}
+			else {
+				score[ch[i] - 65]++;
+			}
+		}
+
+		for (int i = 1; i < 26; i++) {
+			if (score[index] < score[i]) {
+				index = i; // 현재 i값으로 저장
+				flag = 0; // 플래그가 0이 아닌, 1이상일 경우 ? 출력하기 위함
+			}
+			else if (score[index] > score[i]) {
+				continue;
+			}
+			else {
+				flag++;
+			}
+		}
+
+		if (flag == 0) {
+			printf("%c", index + 65);
+		}
+
+		else if (flag != 0) {
+			printf("?");
+		}
+		
+
+		return 0;
+	}
