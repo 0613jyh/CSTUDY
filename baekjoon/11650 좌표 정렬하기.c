72619efce0b3ef2a22df9f7f8d6e3ea23d152ae7
0613jyh <130746525+0613jyh@users.noreply.github.com>
2023-11-19 22:31:55 +0900

    Create 11650 좌표 정렬하기.c

---
+++ baekjoon/11650 좌표 정렬하기.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define max 100000
+
+typedef struct {
+	int x;
+	int y;
+}xy;
+
+int compare(const void *a, const void *b) {
+
+	xy A = *(xy*)a;
+	xy B = *(xy*)b;
+
+	if (A.x < B.x) {
+		return -1;
+	}
+	if (A.x > B.x) {
+		return 1;
+	}
+
+	else {
+		if (A.y > B.y) {
+			return 1;
+		}
+		else if (A.y < B.y) {
+			return -1;
+		}
+	}
+
+}
+
+
+int main() {
+
+	int n;
+	scanf("%d", &n);
+
+	xy* arr;
+	arr = (xy*)malloc(sizeof(xy) * n);
+	for (int i = 0; i < n;i++) {
+		scanf("%d %d", &arr[i].x, &arr[i].y);
+	}
+	qsort(arr, n, sizeof(xy), compare);
+	for (int i = 0; i < n; i++) {
+		printf("%d %d\n", arr[i].x, arr[i].y);
+	}
+
+	free(arr);
+
+	return 0;
+}
